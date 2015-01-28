29795540f8e955af5d84c038cbf9858055db0461
Raphaël Ruet <raphael.ruet@icloud.com>
2015-01-28 09:59:38 +0100

    [CREATION] Création de la classe de Test pour la search activity

---
+++ app/src/androidTest/java/com/eseoteam/android/tagfinder/SearchActivityTest.java
@@ -0,0 +1,19 @@
+package com.eseoteam.android.tagfinder;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created on 28/01/15.
+ * Class test of Mathematician
+ * @author Raphael RUET
+ * @version 0.1.
+ */
+public class SearchActivityTest extends ActivityInstrumentationTestCase2 {
+
+
+
+    public SearchActivityTest()
+    {
+        super(SearchActivity.class);
+    }
+}
