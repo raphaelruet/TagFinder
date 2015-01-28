187c2cdcc3ffbbdbf28fcdb272e4d3b1c677aa15
Raphaël Ruet <raphael.ruet@icloud.com>
2015-01-28 14:37:26 +0100

    [DEPLACEMENT] Ajout d'un toast pour éviter de lancer une recherche sans connexion wifi

--- app/src/main/java/com/eseoteam/android/tagfinder/InfoTagActivity.java
+++ app/src/main/java/com/eseoteam/android/tagfinder/InfoTagActivity.java
@@ -1,15 +1,21 @@
 package com.eseoteam.android.tagfinder;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created on 15/01/15.
@@ -21,19 +27,30 @@ import android.widget.TextView;
 
 public class InfoTagActivity extends ActionBarActivity {
 
-    //Fields of the textView
+    /**
+     * Fields of the textView
+     */
     TextView textTagName;
     TextView textTagId;
     TextView textTagData;
     TextView textTimestamp;
 
-    //Fields in the database
+    /**
+     * Fields in the database
+     */
     private static final String TAG_NAME = "tag_name";
     private static final String TAG_MID = "tag_mid";
     private static final String TAG_DATA = "tag_data";
     private static final String COLUMN_TIME_STAMP = "timestamp";
 
-    //Id in the database
+    /**
+     * The X position of the toasts from the top of the screen
+     */
+    private int toastYPosition;
+
+    /**
+     * Id in the database
+      */
     private long idInDatabase;
 
     /**
@@ -49,6 +66,10 @@ public class InfoTagActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_tag);
         getSupportActionBar().hide();
+
+        // Récupération des dimensions de l 'écran afin de régler la position des toasts
+        DisplayMetrics displayMetrics = getApplicationContext().getResources().getDisplayMetrics();
+        toastYPosition = (displayMetrics.heightPixels/2);
 
         //Sets the listener
         setListeners();
@@ -106,10 +127,18 @@ public class InfoTagActivity extends ActionBarActivity {
     private View.OnClickListener searchButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            //TODO Demarrer la recherche pour le tag avec l'id passé
+        if(isWifiEnabled()) {
+            //Go to the search activity.
             Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
             intent.putExtra("tag_id_in_db",idInDatabase);
-            startActivity(intent);
+            startActivity(intent);        }
+        else {
+            //Notify user that the wifi isn't enabled.
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    R.string.toast_no_wifi2, Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM,0,toastYPosition);
+            toast.show();
+        }
         }
     };
 
@@ -183,5 +212,17 @@ public class InfoTagActivity extends ActionBarActivity {
             dialog.cancel();
         }
     };
+
+    /**
+     * Checks if the Wifi is enabled
+     * @return True if the Wifi is enabled else false.
+     */
+    private boolean isWifiEnabled() {
+        //Retrieve the network state.
+        ConnectivityManager connManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return mWifi.isConnected();
+    }
 
 }
