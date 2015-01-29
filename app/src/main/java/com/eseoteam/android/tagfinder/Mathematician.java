bbb563b0e58376e38e713bbde932de1fe53d3bb8
Pierre TOUZE <touzepierre@gmail.com>
2015-01-29 11:40:56 +0100

    [MODIFICATION] Correction de l'area dans  tagZoneSelection (problème de > <)

--- app/src/main/java/com/eseoteam/android/tagfinder/Mathematician.java
+++ app/src/main/java/com/eseoteam/android/tagfinder/Mathematician.java
@@ -1,27 +1,40 @@
 package com.eseoteam.android.tagfinder;
 
-import android.util.Log;
-
 import java.util.ArrayList;
 
 /**
  * Created on 22/01/15.
  *
  * Algorithms to determine the right direction depending on the RSSI level and angle
- *
- * @author Charline LEROUGE.
- * @version 0.1.
+ * @author Charline LEROUGE, Pierre TOUZE.
+ * @version 0.2.
  */
 public class Mathematician {
 
+    /**
+     * Number of degrees in a circle.
+     */
     private static final int ANGLE = 360;
+
+    /**
+     * Number of rows in the matrix containing the data.
+     */
     private static final int ROW = 3;
 
+    /**
+     * The minimum value of rssi.
+     */
     private static final int MIN_RSSI = -80;
 
+    /**
+     * The matrix containing the rssi, readCount in function of the angle.
+     */
     private int[][] angleTable;
 
-    private ArrayList<Zone> zoneList = new ArrayList<>();
+    /**
+     * List of the different zone where a tag has been detected.
+     */
+    private ArrayList<Zone> zoneList;
 
     /**
      * Constructor of the matrix containing information
@@ -36,6 +49,7 @@ public class Mathematician {
             this.angleTable[1][angle] = MIN_RSSI;
             this.angleTable[2][angle] = 0;
         }
+        zoneList  = new ArrayList<>();
     }
 
     /**
@@ -61,10 +75,20 @@ public class Mathematician {
         this.angleTable[2][angle] = nbPassage;
     }
 
+    /**
+     * Gives the rssi of the searched tag.
+     * @param angle The angle corresponding to the desired rssi.
+     * @return The rssi at the specified angle.
+     */
     public int getRssi(int angle){
        return this.angleTable[1][angle];
     }
 
+    /**
+     * Gives the number of time a rssi has been received at a sepcific angle.
+     * @param angle The angle corresponding to the desired nbPassage.
+     * @return The number of passage.
+     */
     public int getNbPassage(int angle){
         return this.angleTable[2][angle];
     }
@@ -79,7 +103,6 @@ public class Mathematician {
         int minRSSI = minRSSI();
 
         int median = (int)Math.floor((maxRSSI + minRSSI) / 2);
-        Log.e("Math","Min" + minRSSI + " Max " + maxRSSI + " Median "+median);
         for(angle = 0; angle < ANGLE; angle++) {
             if (angleTable[1][angle] > median){
                 angleTable[1][angle] = angleTable[1][angle] - median;
@@ -90,7 +113,7 @@ public class Mathematician {
     }
 
     /**
-     * Calculation of the maximum RSSI value
+     * Calculation of the maximum RSSI value.
      */
     private int maxRSSI(){
         int angle;
@@ -105,7 +128,7 @@ public class Mathematician {
     }
 
     /**
-     * Calculation of the minimum RSSI value
+     * Calculation of the minimum RSSI value.
      */
     private int minRSSI(){
 
@@ -118,34 +141,31 @@ public class Mathematician {
             }
         }
         return minRSSI;
-
     }
 
     /**
      * Split values into possible zones.
      * Select best zone.
-     * @return tab with angles values of the searched tag
+     * @return tab with angles values of the searched tag.
      */
     public int[] bestZoneSelection(){
         this.medianAlgorithm();
         int [] angleDirection = new int [2];
         
         splitInZone();
-        
         int indexZone = tagZoneSelection();
 
         //Put angleStart and angleStop in a tab
         angleDirection[0] = zoneList.get(indexZone).getAngleStart();
         angleDirection[1] = zoneList.get(indexZone).getAngleStop();
-
         return angleDirection;
     }
 
 
     /**
-     * Add Zone in ArrayList when RSSI value is positive
+     * Add Zone in ArrayList when RSSI value is positive.
      */
-    private void splitInZone(){
+    public void splitInZone(){
 
         int currentAngle;
         int angleStart = 0 ;
@@ -153,7 +173,6 @@ public class Mathematician {
 
         // Determine angleStart and angleStop of each zone
         for (currentAngle = 0; currentAngle < ANGLE; currentAngle++) {
-            //Log.e("Math","Angle: " + currentAngle + "Rssi: "+ angleTable[1][currentAngle]);
             if (currentAngle == 0) {
                 if (angleTable[1][currentAngle] != 0) {
                     angleStart = currentAngle;
@@ -167,31 +186,25 @@ public class Mathematician {
                     angleStart = currentAngle;
                 }
                 if ((angleTable[1][currentAngle - 1] != 0) && (angleTable[1][currentAngle] == 0)) {
-                    angleStop = currentAngle;
+                    angleStop = currentAngle-1;
                 }
             }
-
             // Create zone and put them in an ArrayList
-
             if(angleStart!= angleStop && angleStop!=0) {
 
                 Zone zone = new Zone(angleStart, angleStop, angleStop - angleStart) ;
-                Log.e("Math","Start "+ angleStart + " End "+angleStop);
                 zoneList.add(zone);
 
                 angleStart = 0;
                 angleStop = 0;
             }
-
         }
-
     }
 
-
     /**
-     * Determination of the zone with the biggest area
+     * Determination of the zone with the biggest area.
      */
-    private int tagZoneSelection(){
+    public int tagZoneSelection(){
         int sumRssi =0;
         int biggestArea =0;
         int indexZone =0;
@@ -200,16 +213,14 @@ public class Mathematician {
         for (int indexOfZone=0; indexOfZone<zoneList.size(); indexOfZone++) {
             for(int j=zoneList.get(indexOfZone).getAngleStart(); 
                 j<=zoneList.get(indexOfZone).getAngleStop();j++ ){
-                
                 sumRssi += angleTable[1][j];
             }
-            
-            if (sumRssi*zoneList.get(indexOfZone).getAngleSize() < biggestArea){
+            if (sumRssi*zoneList.get(indexOfZone).getAngleSize() > biggestArea){
                 biggestArea = sumRssi*zoneList.get(indexOfZone).getAngleSize();
                 indexZone = indexOfZone;
             }
+            sumRssi = 0;
         }
         return indexZone;
     }
-
 }
