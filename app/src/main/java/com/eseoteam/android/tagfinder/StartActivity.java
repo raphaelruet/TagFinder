e4e7d21eeff11e8bcf9e48597a9980271bf88448
Raphaël Ruet <raphael.ruet@icloud.com>
2015-02-02 18:52:24 +0100

    [MODIFICATION] Ajout de commentaires et nettoyage

--- app/src/main/java/com/eseoteam/android/tagfinder/StartActivity.java
+++ app/src/main/java/com/eseoteam/android/tagfinder/StartActivity.java
@@ -4,10 +4,16 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
+/**
+ * Manage the Start screen
+ * Created on 07/01/2014.
+ * @author Raphael RUET.
+ * @version 0.2
+ */
 public class StartActivity extends ActionBarActivity {
+
+    // Attributes //
 
     /**
      * SplashScreen duration in millisecond.
@@ -19,6 +25,13 @@ public class StartActivity extends ActionBarActivity {
      */
     private static final boolean ENABLE_TIMER = true;
 
+
+    // Methods //
+
+    /**
+     * The onCreate method
+     * @param savedInstanceState the state of the savedInstance
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,28 +56,5 @@ public class StartActivity extends ActionBarActivity {
             // Close this activity
             finish();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
